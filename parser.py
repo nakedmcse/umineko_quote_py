f0879c03fbdb2a5ee38596b1573389d06d160cf0
Walker Aldridge <daldridge@lairdscomputer.com>
2026-02-01 22:41:20 -0600

    Optimize parser - inline cleanups as replaces, reduce and optimize processing regexes

--- parser.py
+++ parser.py
@@ -4,26 +4,16 @@ from dataclasses import dataclass
 from typing import List
 
 # Parsing Regexes
-dialog_line_regex = re.compile(r'^d2? \[lv')
 voice_meta_regex = re.compile(r'\[lv 0\*"(\d+)"\*"(.*)"\]')
 narrator_line_regex = re.compile(r'^d2? `')
 bracket_regex = re.compile(r'\[[^\]]*\]')
 episode_marker_regex = re.compile(r'^new_(?:tea|ura|episode) (\d+)\r?$')
-cleanup_patterns = [
-    "`[@]", "`[\\]", "`[|]", "`\"", "\"`",
-    "[@]", "[\\]", "[|]", "\"\""
-]
 text_rules = [
-    (re.compile(r'\{n\}'), ' '),
-    (re.compile(r'\{c:([A-Fa-f0-9]+):([^}]+)\}'), r'\2'),
-    (re.compile(r'\{f:\d+:([^}]+)\}'), r'\1'),
-    (re.compile(r'\{p:\d{2,}:([^}]+)\}'), r'\1'),
-    (re.compile(r'\{p:1:([^}]+)\}?'), r'\1'),
-    (re.compile(r'\{p:2:([^}]+)\}?'), r'\1'),
+    (re.compile(r'\{[cef]:[A-Fa-f0-9]+:([^}]+)\}'), r'\1'),
+    (re.compile(r'\{p:\d+:([^}]+)\}'), r'\1'),
     (re.compile(r'\{ruby:([^:]+):([^}]+)\}'), r'\2 (\1)'),
     (re.compile(r'\{i:([^}]+)\}'), r'\1'),
     (re.compile(r'\{[a-z]+:[^}]*\}'), ''),
-    (re.compile(r'\`'), ''),
 ]
 
 # Data Classes
@@ -36,16 +26,13 @@ class ParsedQuote:
 
 # Parser
 def extract_text(text: str) -> str:
-    for p in cleanup_patterns:
-        text = text.replace(p, "")
-    text = bracket_regex.sub('', text).removeprefix("d2 ").removeprefix("d ").strip().strip('`"')
+    text = text.replace('{n}', ' ').replace('`','').replace('"','')
+    text = bracket_regex.sub('', text).removeprefix("d2 ").removeprefix("d ").strip()
     for p in text_rules:
         text = p[0].sub(p[1], text)
     return text
 
 def parse_line(line: str) -> ParsedQuote | None:
-    if not dialog_line_regex.match(line):
-        return None
     all_matches = voice_meta_regex.findall(line)
     if not all_matches or len(all_matches[0]) < 2:
         return None
@@ -60,11 +47,12 @@ def parse_line(line: str) -> ParsedQuote | None:
 
     text = extract_text(line)
     if text == "":
+        print(line)
         return None
 
     return ParsedQuote(text, character_id, episode, list(audio_ids))
 
 def parse_all_quotes(filename: str) -> List[ParsedQuote]:
     with open(filename, "r") as file:
-        quotes = [y for y in (parse_line(x) for x in file.read().splitlines()) if y is not None]
+        quotes = [y for y in (parse_line(x) for x in file.read().splitlines() if x.startswith(("d [lv","d2 [lv"))) if y is not None]
     return quotes
--- umineko_quote.py
+++ umineko_quote.py
@@ -8,7 +8,7 @@ from utils import get_size,sizeWords
 start = time.time()
 parsed = parse_all_quotes('umineko.txt')
 end = time.time()
-print(f"Parsed quotes ({sizeWords(get_size(parsed))}) in {(end-start)*1000:.4f} ms")
+print(f"Parsed {len(parsed)} quotes ({sizeWords(get_size(parsed))}) in {(end-start)*1000:.4f} ms")
 
 start = time.time()
 index = build_index(parsed)
