e0463989cf508293cdd884a3d844b5e03ca9cb45
Walker Aldridge <daldridge@lairdscomputer.com>
2026-02-03 17:08:23 -0600

    Rewrite parser

--- parser.py
+++ parser.py
@@ -4,17 +4,9 @@ from dataclasses import dataclass
 from typing import List
 
 # Parsing Regexes
-voice_meta_regex = re.compile(r'\[lv 0\*"(\d+)"\*"(.*)"\]')
-narrator_line_regex = re.compile(r'^d2? `')
-bracket_regex = re.compile(r'\[[^\]]*\]')
-episode_marker_regex = re.compile(r'^new_(?:tea|ura|episode) (\d+)\r?$')
-text_rules = [
-    (re.compile(r'\{[cef]:[A-Fa-f0-9]+:([^}]+)\}'), r'\1'),
-    (re.compile(r'\{p:\d+:([^}]+)\}'), r'\1'),
-    (re.compile(r'\{ruby:([^:]+):([^}]+)\}'), r'\2 (\1)'),
-    (re.compile(r'\{i:([^}]+)\}'), r'\1'),
-    (re.compile(r'\{[a-z]+:[^}]*\}'), ''),
-]
+inline_command = re.compile(r'\[([^\]]+)\]')
+inline_text = re.compile(r'`([^`]+)`')
+inline_voice = re.compile(r'lv 0\*"(\d+)"\*"(.*)"')
 
 # Data Classes
 @dataclass
@@ -25,34 +17,75 @@ class ParsedQuote:
     audio_ids: List[int]
 
 # Parser
-def extract_text(text: str) -> str:
-    text = text.replace('{n}', ' ').replace('`','').replace('"','')
-    text = bracket_regex.sub('', text).removeprefix("d2 ").removeprefix("d ").strip()
-    for p in text_rules:
-        text = p[0].sub(p[1], text)
-    return text
+def apply_format(string: str) -> str:
+    retval = []
+    first_idx = string.find('{')
+    if first_idx == -1:
+        return string
+    if '}' not in string:
+        return string
+    last_idx = len(string) - string[::-1].find('}') -1
+    if first_idx != 0:
+        retval.append(string[:first_idx-1]) # before formatting
 
-def parse_line(line: str) -> ParsedQuote | None:
-    all_matches = voice_meta_regex.findall(line)
-    if not all_matches or len(all_matches[0]) < 2:
+    extract = string[first_idx+1:last_idx-1]
+    if '{' in extract:
+        extract = apply_format(extract) # recurse
+
+    # parse formatting
+    split = extract.split(':')
+    match len(split):
+        case 1:
+            extract = extract.replace("n"," ").replace("0","").replace("qt","\"").replace("-","")
+            extract = extract.replace("ob","(").replace("eb",")")
+            extract = extract.replace("os","[").replace("es","]")
+        case 2:
+            extract = split[1]
+        case 3:
+            if split[0] != 'y':
+                extract = split[2]
+            else:
+                extract = ""
+    retval.append(extract)
+
+    if last_idx < len(string)-1:
+        retval.append(string[last_idx+1:]) # after formatting
+    return "".join(retval)
+
+def parse_formatting(strings: List[str]) -> str:
+    formatted = []
+    for s in strings:
+        if '{' not in s:
+            formatted.append(s)
+            continue
+        formatted.append(apply_format(s))
+    return "".join(formatted)
+
+def parse_dline(line: str) -> ParsedQuote | None:
+    quote = ParsedQuote("", 100, -1, [])
+    text = inline_text.findall(line)
+    if not text:
         return None
+    quote.text = parse_formatting(text)
 
-    character_id = int(all_matches[0][0])
-    first_audio_id = all_matches[0][1]
-    episode = int(first_audio_id[0]) if first_audio_id[0].isdigit() else -1
-
+    commands = inline_command.findall(line)
+    if not commands:
+        return quote
     audio_ids = set()
-    for match in all_matches:
-        audio_ids.add(match[1])
 
-    text = extract_text(line)
-    if text == "":
-        print(line)
-        return None
+    for command in commands:
+        matches = inline_voice.findall(command)
+        if not matches:
+            continue
+        quote.character_id = int(matches[0][0])
+        audio_id = matches[0][1]
+        quote.episode = int(audio_id[0]) if audio_id[0].isdigit() else -1
+        audio_ids.add(audio_id)
 
-    return ParsedQuote(text, character_id, episode, list(audio_ids))
+    quote.audio_ids = list(audio_ids)
+    return quote
 
 def parse_all_quotes(filename: str) -> List[ParsedQuote]:
     with open(filename, "r") as file:
-        quotes = [y for y in (parse_line(x) for x in file.read().splitlines() if x.startswith(("d [lv","d2 [lv"))) if y is not None]
+        quotes = [y for y in (parse_dline(x) for x in file.read().splitlines() if x.startswith(("d ","d2 "))) if y is not None]
     return quotes
